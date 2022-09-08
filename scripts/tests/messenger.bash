3c531c81fb5766668fdc5cb49bf666ed3c338d69
munair <munair@gmail.com>
2022-09-08 08:57:38 -0400

    minor beautification

--- scripts/tests/messenger.bash
+++ scripts/tests/messenger.bash
@@ -12,9 +12,9 @@
 # python3 messenger.py "Sending a test message from a BASH script."
 
 if [ -z "$1" ] ; then
-    echo -e "\nNormal usage: \n\tbash messenger.bash 'message' \nBut no argument supplied. Let's gather it now... "
+    echo -e "\nNormal usage: \n\tbash messenger.bash 'message' \n\nBut no argument supplied. Let's gather it now... "
     message="\"Sending a test message from a BASH script that is a wrapper around a Python module (messenger.py).\""
-    read -p "type a replacement value or press enter to continue with default argument [$message]: " message && message=${message:-sending_a_test_message_from_a_BASH_script}
+    read -p "Type a replacement value or press enter to continue with default argument [$message]: " message && message=${message:-sending_a_test_message_from_a_BASH_script}
 
 else
     message=$1
