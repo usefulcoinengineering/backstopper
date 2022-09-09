a23ce6bdd9bd4791504544a4437f9330d9a5e8d0
munair <munair@gmail.com>
2022-09-09 19:19:26 -0400

    try to accept command line values

--- scripts/backstopper.bash
+++ scripts/backstopper.bash
@@ -6,20 +6,34 @@
 # script purpose: start or stop a Gemini backstopper bot
 # script argument: the action desired [start/stop]
 
+# check for running bots/processes.
 echo -e "\nchecking for active backstopper bots... "
 ps auwx | grep -e "backstopper" | grep -v "grep" && echo -e "\t...there are active backstopper bots running.\n "
 if [ $? == "1" ]; then echo -e "\t...no active bots found...\n " ; fi
 
-read -p "start/stop trading bot [start]: " action
-action=${action:-start}
-
+# do we kill existing processes or start a new bot?
+action="start" && read -p "start/stop trading bot [$action]: " enteredvalue && action=${enteredvalue:-$action}
 if [ $action == "stop" ]; then kill -s KILL $(ps auwx | grep -e "backstopper" | grep -v "grep" | awk '{print $2}') ; fi
 if [ $action == "start" ]; then 
-    pair='ETHUSD'
-    size='0.0010'
-    stop='0.0010'
-    sell='0.0010'
-    cd $(find / -type d -name "backstopper" 2>/dev/null | head -1)
-    git pull
+
+    # either use arguments.
+    if [ $# == "5" ]; then
+        pair=$1
+        size=$2
+        stop=$3
+        sell=$4
+    else
+        # or confirm/define default values.
+        pair="ETHUSD" && read -p "specify asset pair [the default value is $pair]: " enteredvalue && pair=${enteredvalue:-$pair}
+        size="0.0010" && read -p "specify trade size [the default value is $size]: " enteredvalue && size=${enteredvalue:-$size}
+        stop="0.0010" && read -p "specify price stop [the default value is $stop]: " enteredvalue && stop=${enteredvalue:-$stop}
+        sell="0.0010" && read -p "specify price sell [the default value is $sell]: " enteredvalue && sell=${enteredvalue:-$sell}
+        echo -e "\ngoing to execute:\n\npython3 -m backstopper $pair $size $stop $sell\n\n"
+    fi
+    
+    # get latest version.
+    cd $(find / -type d -name "backstopper" 2>/dev/null | head -1) && git pull
+
+    # execute latest version.
     python3 -m backstopper $pair $size $stop $sell
 fi
