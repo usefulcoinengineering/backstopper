7afc9b06722073e25d4ba30396272414cfb3df7b
munair <munair@gmail.com>
2022-09-09 14:05:51 -0400

    fix bot searching capacity

--- scripts/backstopper.bash
+++ scripts/backstopper.bash
@@ -6,14 +6,14 @@
 # script purpose: start or stop a Gemini backstopper bot
 # script argument: the action desired [start/stop]
 
-echo "checking for active bots... "
-ps auwx | grep -e "bash [b]" -e "python3 \." | grep -v "backstopper.bash" && echo "there are active bots running. "
-if [ $? == "1" ]; then echo "no active bots found... " ; fi
+echo -e "\nchecking for active bots... "
+ps auwx | grep -e "backstopper" | grep -v "grep" && echo -e "\tthere are active bots running. "
+if [ $? == "1" ]; then echo -e "\tno active bots found... " ; fi
 
 read -p "start/stop trading bot [start]: " action
 action=${action:-start}
 
-if [ $action == "stop" ]; then kill -s KILL $(ps auwx | grep -E 'bash [b]|python3 \.' | awk '{print $2}') ; fi
+if [ $action == "stop" ]; then kill -s KILL $(ps auwx | grep -e "backstopper" | grep -v "grep" | awk '{print $2}') ; fi
 if [ $action == "start" ]; then 
     pair='ETHUSD'
     size='0.0010'
