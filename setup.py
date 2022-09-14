665de2ce509ddbea0864bf12a75f65a313db1dce
munair <munair@gmail.com>
2022-09-14 11:00:35 -0400

    experimenting with long_description.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='backstopper', version='1.0', packages=find_packages())
+setup(name='backstopper', version='1.0', packages=find_packages(), long_description=open('README.md').read())
