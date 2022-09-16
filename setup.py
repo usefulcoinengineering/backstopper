fefa5a7f3d6132faeacd0d5e1e3ba2b97dfb3f80
munair <munair@gmail.com>
2022-09-16 11:28:37 -0400

    add a little more info to the setup file.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='backstopper', version='1.0', packages=find_packages(), long_description=open('README.md').read())
+setup(  name='backstopper', 
+        version='1.0', 
+        summary='gemini backstopper trading bot', 
+        homepage='https://www.usefulcoin.com', 
+        license=open('LICENSE').read(),
+        packages=find_packages(), 
+        long_description=open('README.md').read()
+)
